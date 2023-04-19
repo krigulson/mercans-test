45eba86735c995a1313514bd537b7d1269ea1dc1
Eduard Krigulson <eduard@weekdone.com>
2023-04-19 11:43:27 +0300

    Create sidebar component

---
+++ src/components/NavigationSidebar.vue
@@ -0,0 +1,18 @@
+<script>
+
+export default {
+  data() {
+    return {
+      menuLinks: menuLinks
+    }
+  },
+
+
+}
+</script>
+<template>
+  <aside class="sidebar">
+    <ul>
+    </ul>
+  </aside>
+</template>
