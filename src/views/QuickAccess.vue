2aed5edc0d859c7400fb3eb5e7581fdc932c70dd
Eduard Krigulson <eduard@weekdone.com>
2023-04-19 18:03:24 +0300

    Create helper and view file for routes

---
+++ src/views/QuickAccess.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+import humanize from '../helpers';
+
+defineProps({
+  title: {
+    type: String,
+    required: true
+  }
+})
+
+</script>
+
+<template>
+  <h1 class="heading">{{ humanize(title) }}</h1>
+</template>
