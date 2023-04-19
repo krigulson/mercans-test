2917615095fa54b775ac4100706f58f3a1a20e64
Eduard Krigulson <eduard@weekdone.com>
2023-04-19 12:06:16 +0300

    Sidebar heading and more sidebar styling

--- src/components/NavigationSidebar.vue
+++ src/components/NavigationSidebar.vue
@@ -31,6 +31,7 @@ export default {
 
 <template>
   <aside class="sidebar">
+    <div class="sidebar-heading">Employee self service</div>
     <ul>
       <li v-for="sortedLinks in sortedLinks">
         <a :href="sortedLinks.path">{{ humanize(sortedLinks.title) }}</a>
@@ -42,8 +43,16 @@ export default {
 <style scoped>
 .sidebar {
   height: 100vh;
+  max-width: 230px;
   border-right: 1px solid var(--dark-gray);
   background: var(--white);
+}
+
+.sidebar-heading {
+  padding: 20px 110px 20px 36px;
+  background: #001738 0% 0% no-repeat padding-box;
+  font-weight: 700;
+  color: var(--white);
 }
 
 .sidebar ul {
@@ -51,23 +60,25 @@ export default {
   margin-block-start: 0;
   margin-block-end: 0;
   padding-inline-start: 0;
-
-  padding-left: 36px;
-  padding-right: 30px;
+  padding-top: 40px;
 }
 
 .sidebar ul li {
-  padding-top: 10px;
-  padding-bottom: 10px;
   height: 40px;
 }
 
+.sidebar ul li:hover {
+  background: var(--light-grayish-blue) 0% 0% no-repeat padding-box;
+  cursor: pointer;
+}
+
 .sidebar ul li a {
+  display: block;
   color: var(--black);
   text-align: left;
   font-size: 14px;
   letter-spacing: 0px;
-  color: #001738;
   opacity: 1;
+  padding: 10px 35px;
 }
 </style>
