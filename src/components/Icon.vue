dd579d3e668302916e394c65899808780fa25d5d
Eduard Krigulson <eduard@weekdone.com>
2023-04-20 13:33:22 +0300

    Add icons

---
+++ src/components/Icon.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { defineAsyncComponent } from 'vue';
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  width: {
+    type: Number,
+    required: true
+  },
+  height: {
+    type: Number,
+    required: true
+  },
+});
+
+const icon = defineAsyncComponent(() => import(`../assets/icons/${props.name}.svg`));
+</script>
+
+<template>
+  <component :is="icon" />
+</template>
